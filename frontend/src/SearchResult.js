350add7a1fa28c6fa2ec72da08e9088b59359dba
정서린 <tjfls2004@gmail.com>
2023-08-30 21:57:39 +0900

    intersectionobserver

--- frontend/src/SearchResult.js
+++ frontend/src/SearchResult.js
@@ -22,33 +22,33 @@ class SearchResult {
     this.render();
   }
 
-  isElementInviewport(el) {
-    const rect = el.getBoundingClientRect();
-    return (
-      rect.top > 0 &&
-      rect.left > 0 &&
-      rect.bottom <
-        (window.innerHeight || document.documentElement.clientHeight) &&
-      rect.right < (window.innerWidth || document.documentElement.clientWidth)
-    );
-  }
+  listObserver = new IntersectionObserver((items, observer) => {
+    items.forEach((item) => {
+      // 아이템이 화면에 보일 때
+      if (item.isIntersecting) {
+        // 이미지를 로드한다
+        item.target.querySelector("img").src =
+          item.target.querySelector("img").dataset.src;
 
-  applyEventToElement = (items) => {
-    document.addEventListener("scroll", () => {
-      items.forEach((el, index) => {
-        if (this.isElementInviewport(el) && items.length - 1 === index) {
+        // 마지막 요소를 찾아낸다
+        // console.log(this.data.length);
+        let dataIndex = Number(item.target.dataset.index);
+        // console.log(dataIndex);
+        if (dataIndex + 1 === this.data.length) {
+          console.log("마지막");
+          // 마지막 요소라면? nextPage 호출
           this.onNextPage();
         }
-      });
+      }
     });
-  };
+  });
 
   render() {
     this.$searchResult.innerHTML = this.data
       .map(
-        (cat) => `
-          <li class="item">
-            <img src='${cat.url}' alt='${cat.name}' />
+        (cat, index) => `
+          <li class="item" data-index=${index}>
+            <img data-src=${cat.url} alt='${cat.name}' />
           </li>
         `
       )
@@ -58,9 +58,8 @@ class SearchResult {
       $item.addEventListener("click", () => {
         this.onClick(this.data[index]);
       });
-    });
 
-    let listItems = this.$searchResult.querySelectorAll(".item");
-    this.applyEventToElement(listItems);
+      this.listObserver.observe($item);
+    });
   }
 }
